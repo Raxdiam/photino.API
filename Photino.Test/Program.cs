484f4f1084052d4e913423aee59f3b17e6c7a27d
Raxdiam <Raxdiam@users.noreply.github.com>
2022-06-25 22:03:19 -0400

    Added test project

---
+++ Photino.Test/Program.cs
@@ -0,0 +1,18 @@
+﻿using PhotinoAPI;
+using PhotinoNET;
+
+namespace PhotinoTest;
+
+internal class Program
+{
+    private static void Main()
+    {
+        var window = new PhotinoWindow()
+            .SetTitle("Test Window")
+            .SetUseApi()
+            .Center()
+            .Load("http://localhost:3000");
+
+        window.WaitForClose();
+    }
+}
